c27e95fb2ff5791a5ad9c28ba8b16530d3496b82
Helderfox49 <mbottoguyzo2012@gmail.com>
2026-06-02 10:51:11 +0100

    Initialisation du projet

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose =  require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+//Route de test
+app.get("/api/ping", (req, res) =>{
+    res.json({
+        message: "Serveur TaskFlow operationnel \n"
+    })
+});
+
+
+
+//Vérification à la connexion de Mongo DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connecté avec succès.");
+
+        // Le serveur ne démarre que si la connexion à la base de données réussit
+        app.listen(process.env.PORT, () => {
+            console.log(`Serveur démarré sur le port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Erreur de connexion à MongoDB :", err.message);
+    });
